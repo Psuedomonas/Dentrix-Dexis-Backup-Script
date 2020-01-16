aa54073e4d2c41cfd81f92275f6b472cfd91e35f
Pseudomonas <zehm0007@d.umn.edu>
2020-01-15 20:14:26 -0600

    Update AutomateBackup.ps1

--- AutomateBackup.ps1
+++ AutomateBackup.ps1
@@ -1,27 +1,38 @@
+## 
+## May not use, may make an .exe or something the user clicks on instead
 #New-ScheduledTask
-
 #? Start-ScheduledTask
 #? Stop-ScheduledTask
 #? Wait-Event
+##
 
-#Make Dentrix backup directory
+## Make Dentrix backup directory ##
 $time = Get-Date -Format "MM.dd.yyyy"
 $name = "dentrix" + $time
 New-Item -Path "c:\" -Name $name -ItemType "directory"
 
+## Potentially automate cmds in Dexis and/or Dentrix ##
+## Unmodified code for stack overflow ##
+#$wshell = New-Object -ComObject wscript.shell;
+#$wshell.AppActivate('title of the application window')
+#Sleep 1
+#$wshell.SendKeys('~')
+
+## Perform Backup ##
 #Copy-Item
 
-#Make Dexis backup directory
+## Make Dexis backup directory ##
 $name = "dexis" + $time
 New-Item -Path "c:\" -Name $name -ItemType "directory"
 
+## Potentially automate cmds in Dexis and/or Dentrix ##
+## Unmodified code for stack overflow ##
+#$wshell = New-Object -ComObject wscript.shell;
+#$wshell.AppActivate('title of the application window')
+#Sleep 1
+#$wshell.SendKeys('~')
+
 #Copy-Item
 
+## When backup is complete, shut down computer ##
 #Stop-Computer
-
-#
-$wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('title of the application window')
-Sleep 1
-$wshell.SendKeys('~')
-#
