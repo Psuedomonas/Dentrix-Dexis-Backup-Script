f0fd09878208417712d7e5ab945239f4ac0c3ba0
Pseudomonas <zehm0007@d.umn.edu>
2020-01-17 02:50:43 -0600

    Update AutomateBackup.bat

--- AutomateBackup.bat
+++ AutomateBackup.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-REM launch powershell, bypass security policy, run script with same name as batch file
+REM launch powershell, bypass security policy, run script with same name and directory as batch file
 PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
 
 PAUSE
