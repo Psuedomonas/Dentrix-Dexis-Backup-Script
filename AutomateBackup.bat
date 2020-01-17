b99f0cf01497adc53c20c56a36e7941a6dc1d45f
Pseudomonas <zehm0007@d.umn.edu>
2020-01-16 23:28:23 -0600

    Update AutomateBackup.bat

--- AutomateBackup.bat
+++ AutomateBackup.bat
@@ -1,11 +1,6 @@
 @ECHO OFF
-REM Goto ps1 directory
-REM cd C:\IT
 
 REM launch powershell, bypass security policy, run script with same name as batch file
 PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
 
-REM If admin is needed:
-REM modification of powershell may be requried to accomidate
-REM PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
 PAUSE
