03efc225fcc98e033b79b86251379550f6a19682
Pseudomonas <zehm0007@d.umn.edu>
2020-06-15 02:34:06 -0500

    Update dentrixbak.ps1

--- dentrixbak.ps1
+++ dentrixbak.ps1
@@ -1,1 +1,6 @@
+$dentrix=""
 
+## Perform Dentrix Backup ##
+Write-Host "Performing the Dentrix Backup..."
+Copy-Item $dentrix -Destination $backupDir -Recurse -Force
+Write-Host "Dentrix Backup Complete!"
